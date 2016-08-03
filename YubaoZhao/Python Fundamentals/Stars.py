c3b608ae8451ac8af489c730f4ffb7e5e20b61ef
Yubao Zhao <zhaoyubao1981@gmail.com>
2016-08-03 11:35:11 -0700

    update star.py

--- YubaoZhao/Python Fundamentals/Stars.py
+++ YubaoZhao/Python Fundamentals/Stars.py
@@ -2,7 +2,7 @@
 
 #Part 1
 #Create a function called  draw_stars() that takes a list of numbers and prints out *.
-'''
+
 def draw_stars(arr):
     for num in arr:
         star = ""
@@ -12,16 +12,14 @@ def draw_stars(arr):
 
 x = [4, 6, 1, 3, 5, 7, 25]
 draw_stars(x)
-'''
+
 #Part 2
 #Modify the function above. Allow a list containing integers and strings to be
 #passed to the  draw_stars() function. When a string is passed, instead of
 #displaying *, display the first letter of the string according to the example
 #below. You may use the .lower() string method for this part.
 
-#def draw_stars(arr):
-
-def draw_stars(arr):
+def draw_stars2(arr):
     for item in x:
         if type(item) == int:
             star = ""
@@ -35,4 +33,4 @@ def draw_stars(arr):
             print letter
 
 x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
-draw_stars(x)
+draw_stars2(x)
