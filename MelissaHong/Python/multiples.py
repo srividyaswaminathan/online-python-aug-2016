82c75d7b36f69efdb5875d8d03377264bcd2f87c
Melissa Hong <melissahong1@gmail.com>
2016-08-03 17:01:50 -0700

    added multiply, oddeven, scoresandgrades and modified files in algorithms

--- MelissaHong/Python/multiples.py
+++ MelissaHong/Python/multiples.py
@@ -3,8 +3,16 @@ def odd_print():
   for odds in range (1, 1000):
     if odds % 2 == 1:
       print odds
+debug(odd_print())
 
-#odd_print()
+# for index in range (1, 1000, 2):
+#    print index
+#or
+# if index % 2 != 0:
+#    print index
+
+# odd_print()
+
 
 #prints multiples of 5 from 5 to 1,000,000
 def multiple_5():
