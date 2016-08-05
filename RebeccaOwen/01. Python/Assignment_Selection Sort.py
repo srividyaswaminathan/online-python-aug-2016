1b74e5f857016f81d826ef848c62daa8512967dd
PajamaProgrammer <PajamaProgrammer@gmail.com>
2016-08-04 21:06:34 -0400

    added selection sort

--- RebeccaOwen/01. Python/Assignment_Selection Sort.py
+++ RebeccaOwen/01. Python/Assignment_Selection Sort.py
@@ -20,6 +20,27 @@ def selection_sort(array):
         array[min_loc] = temp  
     return array
 
+def selection_sort_2(array):
+    for min_index in range (0, len(array)/2):
+        max_index = len(array)-1-min_index
+        min_loc = min_index
+        max_loc = max_index
+    
+        for i in range (min_index, max_index):
+            if array[i] < array[min_loc]:
+                min_loc = i
+            if array[len(array)-i-1] > array[max_loc]:
+                max_loc = i
+
+            temp = array[min_index]
+            array[min_index] = array[min_loc]
+            array[min_loc] = temp
+            temp = array[max_index]
+            array[max_index] = array[max_loc]
+            array[max_loc] = temp
+        
+    return array
+
 def create_random_array(n=100, m=10000):
     array = []
     for i in range (n):
@@ -30,6 +51,12 @@ def create_random_array(n=100, m=10000):
 array = create_random_array()
 start = datetime.now()
 print selection_sort(array)
+end = datetime.now()
+delta = end - start
+print '\nTime to sort the array:', delta.total_seconds(), '\n'
+
+start = datetime.now()
+print selection_sort_2(array)
 end = datetime.now()
 delta = end - start
 print '\nTime to sort the array:', delta.total_seconds()
