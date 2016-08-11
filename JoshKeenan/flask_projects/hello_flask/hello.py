7c04ab17fdd374dd25f4cd132d5969628cda7a09
Josh Keenan <josh@joshkeenan.net>
2016-08-11 14:42:11 -0700

    Adds Landing Page assignment

--- JoshKeenan/flask_projects/hello_flask/hello.py
+++ JoshKeenan/flask_projects/hello_flask/hello.py
@@ -1,16 +1,14 @@
-from flask import Flask,render_template  # Import Flask to allow us to create our app.
-app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
-                         # directly, or importing it as a module.
-@app.route('/')          # The "@" symbol designates a "decorator" which attaches the following
-                         # function to the '/' route. This means that whenever we send a request to
-                         # localhost:5000/ we will run the following "hello_world" function.
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
 def hello_world():
-  return 'Hello World!'  # Return 'Hello World!' to the response.
-      
+    return 'Hello World!'
+
 
 @app.route('/success')
-
 def success():
-	return render_template('success.html')
+    return render_template('success.html')
 
 app.run(debug=True)
