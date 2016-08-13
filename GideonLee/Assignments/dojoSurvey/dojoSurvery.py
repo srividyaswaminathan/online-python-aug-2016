381507e5888cb8081b29c6e58aa10e78fcc226b2
Gideon Lee <Gideonxlee@gmail.com>
2016-08-13 13:43:37 -0700

    added validation to Dojo Survey

--- GideonLee/Assignments/dojoSurvey/dojoSurvery.py
+++ GideonLee/Assignments/dojoSurvey/dojoSurvery.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
-
+app.secret_key ='ThisIsSecret'
 
 @app.route('/')
 def index(): 
@@ -9,11 +9,18 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def enterResults():
-	name = request.form['name']
-	email = request.form['email']
-	location = request.form['location']
-	language = request.form['language']
-	comments = request.form['comments']
-	return render_template('result.html', my_name=name, my_email=email, my_comments=comments, my_language=language, my_location=location)
+	if len(request.form['name']) < 1 or len(request.form['email']) < 1 or len(request.form['comments']) < 1:
+		flash('Name, email, and comments cannot be empty!')
+		return redirect('/')
+	if len(request.form['comments']) > 120: 
+		flash('Comments cannot exceed more than 120 characters.')
+		return redirect('/')
+	else: 
+		name = request.form['name']
+		email = request.form['email']
+		location = request.form['location']
+		language = request.form['language']
+		comments = request.form['comments']
+		return render_template('result.html', my_name=name, my_email=email, my_comments=comments, my_language=language, my_location=location)
 
 app.run(debug=True)
