37476488d6358be6ba63e3dd08e50800133a7297
Melissa Hong <melissahong1@gmail.com>
2016-08-15 10:48:06 -0700

    added dojosurvey2

---
+++ MelissaHong/Flask/basic_validation/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, request, session, flash
+app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
+@app.route('/')
+def index():
+  return render_template('index.html')
+@app.route('/process', methods=['POST'])
+def process():
+  if len(request.form['name']) < 1:
+    flash('name cannot be empty!')
+  else:
+    flash('success! your name is {}'.format(request.form['name']))
+  return redirect('/') # either way the application should return to the index and display the message
+app.run(debug=True)
