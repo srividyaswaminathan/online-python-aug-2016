a765ad0b08cd8716871fc1f1cf82f147b99ff37a
Yubao Zhao <zhaoyubao1981@gmail.com>
2016-08-23 10:09:44 -0700

    Refactor ArraysII_01.js

--- YubaoZhao/Algorithms​/Week_04/ArraysII_01.js
+++ YubaoZhao/Algorithms​/Week_04/ArraysII_01.js
@@ -3,19 +3,17 @@
 // Write a function that returns whether the given array has a balance point between indices,
 // where one side’s sum is equal to the other’s. Example: [1,2,3,4,10] → true (between indices 3 & 4), but [1,2,4,2,1] → false.
 function balancePoint(arr) {
-    var balance = false;
-    for(var idx = 1; idx < arr.length; idx++) {
-        var leftSum = 0,
-            rightSum = 0;
-        for(var i = 0; i < idx; i++) {
-            leftSum += arr[i];
-        }
-        for(var j = idx; j < arr.length; j++) {
-            rightSum += arr[j];
-        }
-        if(leftSum == rightSum) {
+    var balance = false,
+        totalSum = 0,
+        leftSum = 0;
+    for(var i = 0; i < arr.length; i++) {
+        totalSum += arr[i];
+    }
+    for(var idx = 0; idx < arr.length-1; idx++) {
+        leftSum += arr[idx];
+        if(leftSum == totalSum - leftSum) {
             balance = true;
-            console.log("Balance Point is between indices [%d] & [%d]", idx-1, idx);
+            console.log("Balance Point is between indices [%d] & [%d]", idx, idx+1);
             break;
         }
     }
@@ -26,16 +24,14 @@ function balancePoint(arr) {
 // where sums are equal on either side (exclude its own value). Return -1 if none exist. Ex.: [- 2,5,7,0,3] → 2, but [9,9] → -1.
 function balanceIndex(arr) {
     var balance = -1;
-    for(var idx = 1; idx <arr.length-1; idx++) {
-        var leftSum = 0,
-            rightSum = 0;
-        for(var i = 0; i < idx; i++) {
-            leftSum += arr[i];
-        }
-        for(var j = idx+1; j < arr.length; j++) {
-            rightSum += arr[j];
-        }
-        if(leftSum == rightSum) {
+    var totalSum = 0,
+        leftSum = 0;
+    for(var i = 0; i <arr.length; i++) {
+        totalSum += arr[i];
+    }
+    for(var idx = 1; idx < arr.length-1; idx++) {
+        leftSum += arr[idx-1];
+        if(leftSum == totalSum-leftSum-arr[idx]) {
             balance = idx;
             console.log("Balance Index is [%d]", idx);
             break;
