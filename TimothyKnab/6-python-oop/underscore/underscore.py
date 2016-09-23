fdef14f539ba17d57575cbec7eee7b4689900e9b
travelingmindful <info@sasquatchcreative.com>
2016-09-23 03:49:48 -0700

    added variables which invoke functions to underscore.py

--- TimothyKnab/6-python-oop/underscore/underscore.py
+++ TimothyKnab/6-python-oop/underscore/underscore.py
@@ -11,13 +11,13 @@ class Underscore(object):
 		newList = []
 		for x in range(len(myList)):
 			newList.append(function(myList[x]))
-		print newList
+		return newList
 
 	def reduce(self, myList, function):
 		value = 0
 		for x in range(len(myList)):
 			value += function(myList[x])
-		print value
+		return value
 
 	def find(self, myList, function):
 		value = 0
@@ -26,25 +26,35 @@ class Underscore(object):
 				value = myList[x]
 				print value
 				break
+		return value
 
 	def filter(self, myList, function):
 		newList = []
 		for x in range(len(myList)):
 			if function(myList[x]):
 				newList.append(myList[x])
-		print newList
+		return newList
 
 	def reject(self, myList, function):
 		newList = []
 		for x in range(len(myList)):
 			if not function(myList[x]):
 				newList.append(myList[x])
-		print newList
+		return newList
 
 
 _ = Underscore()
-_.filter([1,2,3,4,5,6], lambda x : x % 2 == 0)
-_.map([1,2,3], lambda x : x * 3)
-_.reduce([1,2,3], lambda x: x )
-_.find([1,2,3,4,5,6], lambda x: x % 2 == 0)
-_.reject([1,2,3,4,5,6], lambda x: x % 2 == 0)
+evens = _.filter([1,2,3,4,5,6], lambda x : x % 2 == 0)
+print evens
+
+finds = _.map([1,2,3], lambda x : x * 3)
+print finds
+
+mappings = _.reduce([1,2,3], lambda x: x )
+print mappings
+
+reducing = _.find([1,2,3,4,5,6], lambda x: x % 2 == 0)
+print reducing
+
+rejecting = _.reject([1,2,3,4,5,6], lambda x: x % 2 == 0)
+print rejecting
